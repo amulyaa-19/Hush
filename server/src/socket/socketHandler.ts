b5dfc7576573f2b937913581b2dec27ce67fffac
Amulya Srivastava <srivastava.amulya19@gmail.com>
2025-10-23 02:15:12 +0530

    models completed and cleaned the folder structures

--- server/src/socket/socketHandler.ts
+++ server/src/socket/socketHandler.ts
@@ -1,30 +1,22 @@
-// src/socket/socketHandler.ts
-
 import { Server as SocketServer } from 'socket.io';
 import http from 'http';
 
 export const initializeSocketIO = (server: http.Server) => {
-  // Create a new Socket.IO server instance
   const io = new SocketServer(server, {
     cors: {
-      origin: "*", // Allow all origins for development
+      origin: "*", 
       methods: ["GET", "POST"]
     }
   });
 
-  // Listen for the main 'connection' event
   io.on('connection', (socket) => {
-    console.log(`🔌 User connected: ${socket.id}`);
+    console.log(`User connected: ${socket.id}`);
 
-    // Listen for this specific user to disconnect
     socket.on('disconnect', () => {
-      console.log(`🔌 User disconnected: ${socket.id}`);
+      console.log(`User disconnected: ${socket.id}`);
     });
-    
-    // All other socket event listeners for this user will go here
-    // e.g., socket.on('join-room', ...)
+
   });
 
-  // Return the io instance, in case we need to access it from index.ts
   return io;
 };
