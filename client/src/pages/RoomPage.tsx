a899669ba6137adb332906a09e60274bfe66f712
Amulya Srivastava <srivastava.amulya19@gmail.com>
2025-11-07 23:49:11 +0530

    made basic layout for room page

--- client/src/pages/RoomPage.tsx
+++ client/src/pages/RoomPage.tsx
@@ -1,10 +1,18 @@
 import { io, Socket } from "socket.io-client";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_API_URL;
 
+interface IMessage {
+  authorId: string;
+  text: string;
+}
+
 const RoomPage = () => {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+  const [currentMessage, setCurrentMessage] = useState<string>("");
+
   const { roomId } = useParams();
 
   const socketRef = useRef<Socket | null>(null);
@@ -19,13 +27,42 @@ const RoomPage = () => {
 
       newSocket.emit("join-room", { roomId });
     });
+
+    newSocket.on("new-message", (message: IMessage) => {
+      // react add to previous list instead of overwriting it
+      console.log("new message recieved:", message);
+      setMessages((prevMessages) => [...prevMessages, message]);
+    });
     return () => {
       newSocket.disconnect();
     };
   }, [roomId, BACKEND_URL]);
   return (
-    <div>
-      <h1>Room: {roomId}</h1>
+    <div className="flex flex-col h-screen p-4">
+      <h1 className="text-2xl font-bold mb-4">Room: {roomId}</h1>
+      <div className="grow border border-gray-300 rounded-lg p-4 overflow-y-auto mb-4 bg-gray-50">
+        {messages.map((msg, index) => (
+          <div key={index} className="mb-2">
+            <strong>
+              {msg.authorId === socketRef.current?.id ? "You" : msg.authorId}
+            </strong>
+            : <span>{msg.text}</span>
+          </div>
+        ))}
+      </div>
+      <form className="flex">
+        <input
+          type="text"
+          placeholder="Type your message"
+          className="grow border border-gray-300 rounded-l-lg p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 cursor-pointer text-white p-2 rounded-r-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+        >
+          Send
+        </button>
+      </form>
     </div>
   );
 };
